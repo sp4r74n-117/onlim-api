5f04033673913a90da3fc93e316f260f4da7c103
Felix Kostenzer <Felix.Kostenzer@student.uibk.ac.at>
2016-06-06 22:02:54 +0200

    Added AllTests TestSuite

---
+++ src/test/java/onlim/api/test/AllTests.java
@@ -0,0 +1,19 @@
+package onlim.api.test;
+
+import onlim.api.generator.test.TemplateBuilderTest;
+import onlim.api.parser.test.ParserTest;
+import onlim.api.services.resources.OfferResourceTest;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+@RunWith(Suite.class)
+@SuiteClasses({
+	TemplateBuilderTest.class,
+	ParserTest.class,
+	OfferResourceTest.class
+})
+public class AllTests {
+
+}
